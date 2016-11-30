f2982ec31bcc0b70c7faa67206c0c1861551ad01
Jesus Rodriguez <Foxandxss@gmail.com>
2016-11-30 18:13:26 +0100

    add more shows

--- src/app/shared/in-memory-data.service.ts
+++ src/app/shared/in-memory-data.service.ts
@@ -5,7 +5,9 @@ export class InMemoryDataService implements InMemoryDbService {
     let shows = [
       {id: 1, name: 'Doctor Who', cover: 'http://www.coverwhiz.com/content/Doctor-Who-Series-5.jpg', rating: 1},
       {id: 2, name: 'Game of Thrones', cover: 'http://www.coverwhiz.com/content/Game-Of-Thrones-Season-1.jpg', rating: 1},
-      {id: 3, name: 'Dexter', cover: 'http://www.coverwhiz.com/content/Dexter-Season-8.jpg', rating: 3}
+      {id: 3, name: 'Dexter', cover: 'http://www.coverwhiz.com/content/Dexter-Season-8.jpg', rating: 3},
+      {id: 4, name: 'Family Guy', cover: 'http://www.coverwhiz.com/content/Family-Guy-Season-1.jpg', rating: 2},
+      {id: 5, name: 'The Walking Dead', cover: 'http://www.coverwhiz.com/content/The-Walking-Dead-Season-1.jpg', rating: 2}
     ];
 
     return {shows};
